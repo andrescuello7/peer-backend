86d2d86296e92b44a57a88f20f54eb1d33b0e191
pi-andrescuello <andres.cuello@piconsulting.com.ar>
2024-09-09 10:19:04 -0300

    [UPDATE] code for network peer to peer with net and ws dependencie

--- app.js
+++ app.js
@@ -1,26 +1,198 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const path = require("path")
-const socket = require("socket.io")
+const fs = require("fs");
+const http = require("http");
+const path = require("path");
+const { Server, OPEN } = require("ws");
+const { createServer, createConnection } = require("net");
 
-app.use(cors())
-app.set("port", process.env.PORT || 3000)
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')))
+// Class to manage P2P connections and WebSockets
+class PeerToPeer {
+    constructor() {
+        this.ws = null;
+        this.WS_PORT = process.env.PORT || 3000;
+        this.NET_HOST = process.env.HOST || "127.0.0.1";
+        this.NET_PORT = parseInt(this.WS_PORT) + 1;
+        this.conections = []
+        this.socketsConnecteds = [];
 
-const server = app.listen(app.get("port"), () => {
-    console.log("server in port", app.get("port"))
-})
+        let { serverNet, serverHttp } = this.createServers();
+        this.initializeWebconnectSocket(serverHttp);
 
-const io = socket(server, {
-    origin: "*",
-    methods: ["GET", "POST"]
-})
+        // Start the TCP and HTTP servers
+        serverNet.listen({ host: this.NET_HOST, port: this.NET_PORT }, () => {
+            serverHttp.listen({ host: this.NET_HOST, port: this.WS_PORT }, () => {
+                console.log(`\n- Server Run in ${this.NET_HOST}:${this.NET_PORT}`);
+                console.log(`- WebSite in http://localhost:${this.WS_PORT}`);
+            });
+        });
+        serverNet.on("error", (error) => console.error("Server error:", error.message));
+    }
 
-io.on("connection", (socket) => {
-    socket.on("message", (data) => {
-        io.sockets.emit("new-message", data)
-    })
-})
+    sendMessage(data) {
+        const msgModel = new MessageModel(JSON.parse(data.toString()));
+
+        if (msgModel.type === "CONNECTION") {
+            this.connectSocket(msgModel.message)
+        }
+        if (msgModel.type === "MESSAGE") {
+            this.socketsConnecteds.forEach((socket) => {
+                try {
+                    if (!socket._writableState.ended) {
+                        socket.write(data.toString().trim());
+                    }
+                } catch (error) { console.error(error); }
+            });
+            this.ws.clients.forEach((client) => {
+                if (client.readyState === OPEN) {
+                    client.send(msgModel.formatMessage());
+                }
+            });
+        }
+
+    }
+
+    // Attempt to connect to a peer using the provided credentials
+    connectSocket(credentials) {
+        try {
+            let [P_HOST, P_PORT] = credentials.split(':');
+            P_PORT = parseInt(P_PORT) + 1;
+
+            this.connectPeer(P_HOST, P_PORT, {
+                port: P_PORT,
+                host: P_HOST
+            }, {
+                port: this.NET_PORT,
+                host: this.NET_HOST
+            });
+            console.log(`\x1b[32m[+] Connected to peer\x1b[0m ${P_HOST}:${P_PORT}`);
+        } catch (error) {
+            console.error("Failed to connect to peer:", error);
+        }
+    }
+
+    // Establish connection with a peer and maintain the list of active peers
+    connectPeer(host, port, data, myCredentials) {
+        try {
+            const _socket = createConnection({ host, port }, () => {
+                if (data.length > 0) {
+                    this.conections.concat(data);
+                } else {
+                    const exists = this.conections.some(item => item.port === data.port && item.host === data.host);
+                    if (!exists) {
+                        this.conections.push(data)
+                        this.socketsConnecteds.push(_socket);
+                    } else {
+                        this.ws.clients.forEach((item) => {
+                            let _model = new MessageModel(data)
+                            if (item.readyState === OPEN) item.send(_model.formatMessage())
+                        });
+                    }
+                }
+                _socket.write(JSON.stringify(myCredentials || this.conections));
+            })
+        } catch (error) { }
+    }
+
+    // Connect to multiple peers from the received data
+    connectMultiplePeers(data) {
+        for (const connection of data) {
+            const isCurrentHost = connection.port === process.env.PORT;
+            const isValidConnection = connection.port !== undefined && connection.host !== undefined;
+            const isNotConnected = !this.conections.some(peer => peer.port === connection.port && peer.host === connection.host);
+
+            if (!isCurrentHost && isValidConnection && isNotConnected) {
+                this.connectPeer(
+                    connection.host,
+                    connection.port,
+                    data,
+                    { port: this.NET_PORT, host: this.NET_HOST }
+                );
+            }
+        }
+    }
+
+    // Create the TCP and HTTP servers for P2P and WebSockets
+    createServers() {
+        const serverNet = createServer((socket) => {
+            socket.on("data", (buffer) => {
+                try {
+                    const data = JSON.parse(buffer.toString());
+                    if (data.length) {
+                        this.connectMultiplePeers(data);
+                    } else {
+                        this.connectPeer(socket.remoteAddress, data.port, data);
+                    }
+                } catch (error) {
+                    const data = buffer.toString();
+                    console.log(`\x1b[33m  - \x1b[0m ERROR ${error}${data}`);
+                }
+            });
+            socket.on("close", () => console.log(`Socket closed`));
+        });
+
+        const serverHttp = http.createServer((req, res) => {
+            let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url);
+            let extname = path.extname(filePath);
+            let contentType = "text/html";
+            switch (extname) {
+                case ".css":
+                    contentType = "text/css";
+                    break;
+            }
+            fs.readFile(filePath, (err, data) => {
+                if (err) {
+                    if (err.code == "ENOENT") {
+                        fs.readFile(path.join(__dirname, "public", "404.html"), (err, data) => {
+                            res.writeHead(404, { "Content-Type": "text/html" });
+                            res.end(data, "utf8");
+                        });
+                    } else {
+                        res.writeHead(500);
+                        res.end(`Server Error: ${err.code}`);
+                    }
+                } else {
+                    res.writeHead(200, { "Content-Type": contentType });
+                    res.end(data, "utf8");
+                }
+            });
+        });
+        
+        return { serverHttp, serverNet }
+    }
+
+    // Initialize WebSocket server
+    initializeWebconnectSocket(server) {
+        this.ws = new Server({ server });
+
+        this.ws.on("connection", (socket) => {
+            socket.on("message", (data) => this.sendMessage(data));
+            socket.on("close", () => console.log("WebSocket client disconnected"));
+        });
+    }
+}
+
+// Model for handling and formatting messages
+class MessageModel {
+    constructor({ message, username, voice, host, port, type }) {
+        this.type = type;
+        this.host = host || null;
+        this.port = port || null;
+        this.voice = voice;
+        this.message = message;
+        this.username = username;
+    }
+
+    // Format the message into JSON format
+    formatMessage() {
+        return JSON.stringify({
+            type: this.type,
+            port: this.port,
+            host: this.host,
+            voice: this.voice,
+            message: this.message,
+            username: this.username,
+        });
+    }
+}
+
+// Initialize the P2P server
+new PeerToPeer()
---
+++ public/app.js
@@ -0,0 +1,161 @@
+let stateSticker = false
+let soundState = localStorage.getItem('soundState') | true
+let nameStorage = localStorage.getItem('username') ?? 'Anonymous';
+document.querySelector('.chat[data-chat=person2]').classList.add('active-chat')
+
+const idSound = document.getElementById("idSound")
+const input = document.getElementById("input")
+const stickersCss = document.getElementById("stickersCss")
+const wsocket = new WebSocket('ws://localhost:4000');
+// wsocket.onopen = () => document.getElementById('sendButton').disabled = false;
+// wsocket.onclose = () => console.log('WebSocket cerrado');
+// wsocket.onerror = (error) => console.error('WebSocket error:', error);
+
+let notSound = `<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="text-secondary bi bi-volume-mute" viewBox="0 0 16 16">
+  <path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06M6 5.04 4.312 6.39A.5.5 0 0 1 4 6.5H2v3h2a.5.5 0 0 1 .312.11L6 10.96zm7.854.606a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0"/>
+</svg>`
+
+let sound = `<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="text-secondary bi bi-volume-up" viewBox="0 0 16 16">
+  <path d="M11.536 14.01A8.47 8.47 0 0 0 14.026 8a8.47 8.47 0 0 0-2.49-6.01l-.708.707A7.48 7.48 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303z"/>
+  <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.48 5.48 0 0 1 11.025 8a5.48 5.48 0 0 1-1.61 3.89z"/>
+  <path d="M10.025 8a4.5 4.5 0 0 1-1.318 3.182L8 10.475A3.5 3.5 0 0 0 9.025 8c0-.966-.392-1.841-1.025-2.475l.707-.707A4.5 4.5 0 0 1 10.025 8M7 4a.5.5 0 0 0-.812-.39L3.825 5.5H1.5A.5.5 0 0 0 1 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 7 12zM4.312 6.39 6 5.04v5.92L4.312 9.61A.5.5 0 0 0 4 9.5H2v-3h2a.5.5 0 0 0 .312-.11"/>
+</svg>`
+
+let soundsEnum = [
+    { path: './sound/ap_notify.mp3', name: 'Apple Notification' },
+    { path: './sound/ds_call.mp3', name: 'Discord Call' },
+    { path: './sound/mn_experience.mp3', name: 'Minecraft' },
+    { path: './sound/mr_money.mp3', name: 'Mario Money' },
+    { path: './sound/ds_notify.mp3', name: 'Discord Notification' },
+    { path: './sound/ws_error.mp3', name: 'Windows Error' },
+    { path: './sound/quack.mp3', name: 'Quack' },
+    { path: './sound/cristiano.mp3', name: 'Cristiano R' },
+    { path: './sound/messi.mp3', name: 'Encara Messi' },
+    { path: './sound/no-hay-plata.mp3', name: 'No Hay Plata' },
+    { path: './sound/victoria.mp3', name: 'Winner' },
+    { path: './sound/half.mp3', name: 'Half Haif' },
+    { path: './sound/mn_hurt.mp3', name: 'Minecraft Hurt' },
+    { path: './sound/sumbido.mp3', name: 'sumbido' },
+]
+
+document.getElementById("send").addEventListener("click", () => handleEnter())
+input.addEventListener('keydown', (event) => { if (event.keyCode === 13) handleEnter() });
+
+wsocket.onmessage = (event) => {
+    let { username, message, voice } = JSON.parse(event.data)
+    const body = document.getElementById("body")
+
+    if (username !== nameStorage) {
+        if (localStorage.getItem('sound_state')) {
+            let notify;
+            if (notify) {
+                notify.pause();
+                notify.currentTime = 0;
+            }
+
+            notify = new Audio(voice);
+            notify.play().catch(error => console.warn('Error playing the sound:', error));
+            if (voice == './sound/sumbido.mp3') {
+                let container = document.getElementById("container");
+                container.classList.add("shake");
+                setTimeout(() => { container.classList.remove("shake") }, 1000);
+            }
+        }
+        body.innerHTML += `<div class="bubble you"><b>${username}</b></br>${message}</div>`;
+    } else {
+        body.innerHTML += `<div class="bubble me">${message}</div>`;
+        stickersCss.innerHTML = '<div></div>'
+    }
+};
+
+function handleEnter() {
+    try {
+        if (input.value != "" && input.value != undefined) {
+            wsocket.send(JSON.stringify({
+                port: 4001,
+                host: '127.0.0.1',
+                username: localStorage.getItem('username') ?? 'Anonymous',
+                message: input.value,
+                voice: './sound/ds_notify.mp3',
+                type: "MESSAGE"
+            }));
+            input.value = "";
+        }
+    } catch (error) {
+        console.error('WebSocket no está listo para enviar mensajes.', error);
+    }
+}
+
+function soundSend(sound_music, name) {
+    try {
+        wsocket.send(JSON.stringify({
+            port: 4001,
+            host: '127.0.0.1',
+            username: localStorage.getItem('username') ?? 'Anonymous',
+            message: name,
+            voice: sound_music,
+            type: "MESSAGE"
+        }));
+    } catch (error) {
+        console.error('WebSocket no está listo para enviar mensajes.', error);
+    }
+}
+
+function soundList() {
+    if (stateSticker) {
+        stickersCss.innerHTML = '<div></div>'
+    } else {
+        stickersCss.innerHTML = `
+            <div class="floating-menu">
+                <ul>
+                    ${soundsEnum.map((item, i) => `
+                        <li key=${i} onclick="soundSend('${item.path}','${item.name}')"> ${item.name == 'sumbido' ? 
+                        `<span style="color: rgb(217, 34, 34);">${item.name}</span>` :`<span>${item.name}</span>`}</li>`).join('')}
+                </ul>
+            </div>
+        `
+    }
+    stateSticker = !stateSticker
+}
+
+const saveUsername = () => {
+    const userName = document.getElementById("userName")
+    if (userName.value !== "") {
+        localStorage.setItem('username', userName.value)
+        userName.value = ""
+    }
+    userName.placeholder = localStorage.getItem('username') ?? 'Anonymous'
+}
+
+idSound.addEventListener('click', () => {
+    if (soundState === true) {
+        idSound.innerHTML = notSound
+    } else {
+        idSound.innerHTML = sound
+    }
+    soundState = !soundState
+    localStorage.setItem('soundState', soundState)
+})
+
+const connectPeer = () => {
+    const inputConnection = document.getElementById("peerConnection")
+    try {
+        if (inputConnection.value !== "") {
+            wsocket.send(JSON.stringify({
+                port: 4001,
+                host: '127.0.0.1',
+                username: localStorage.getItem('username') ?? 'Anonymous',
+                message: inputConnection.value,
+                voice: './sound/ds_notify.mp3',
+                type: "CONNECTION"
+            }));
+            inputConnection.placeholder = inputConnection.value
+        }
+    } catch (error) {
+        alert(`Error: Host y el Port ${error}`)
+    }
+    inputConnection.value = ""
+}
+
+saveUsername()
+idSound.innerHTML = sound
